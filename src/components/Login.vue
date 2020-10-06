f307ad08dc4c11d28a4bd7f59005053edcb2cd68
Bowske <adabor6@st.amu.edu.pl>
2020-10-06 14:10:31 +0200

    simple login with sdk init

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login">
+    <v-facebook-login
+      v-model="model"
+      app-id="633593563867021"
+      @sdk-init="handleSdkInit"
+    ></v-facebook-login>
+  </div>
+</template>
+
+<script>
+import VFacebookLogin from "vue-facebook-login-component";
+
+export default {
+  name: "Login",
+  components: {
+    VFacebookLogin,
+  },
+  data: () => ({
+    FB: {},
+    model: {},
+    scope: {},
+  }),
+  methods: {
+    testData(scope) {
+      console.log(scope.connected);
+    },
+    handleSdkInit({ FB, scope }) {
+      this.FB = FB;
+      this.scope = scope;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
